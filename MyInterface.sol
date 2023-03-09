e6753e559a58013d88db186e113dfcceef955107
gomyway007 <123950159+gomyway007@users.noreply.github.com>
2023-03-09 15:52:08 +0800

    Create MyInterface.sol

---
+++ MyInterface.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+interface Icounter{
+    function count() view external returns(uint256);
+    function incr() external;
+}
+
+contract Counter{
+    uint256 public count;
+
+    function incr() external{
+        count++;
+    }
+}
+
+contract MyInterface{
+    function MyGetCount(address _addr) external view returns(uint256){
+        return Icounter(_addr).count();
+    }
+
+    function MyIncr(address _addr) external{
+        Icounter(_addr).incr();
+    }
+}

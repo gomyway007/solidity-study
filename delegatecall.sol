3652d721a8bca4eafbb07b5fb202ac7cce65d09b
gomyway007 <123950159+gomyway007@users.noreply.github.com>
2023-03-09 18:54:23 +0800

    Update delegatecall.sol

--- delegatecall.sol
+++ delegatecall.sol
@@ -6,21 +6,18 @@ contract B {
     // NOTE: storage layout must be the same as contract A
     uint public num;
     address public sender;
-    uint public value;
 
     function setVars(uint _num) public payable {
         num = _num;
         sender = msg.sender;
-        value = msg.value;
     }
 }
 
 contract A {
     uint public num;
     address public sender;
-    uint public value;
 
-    function setVars(address _contract, uint _num) public payable {
+    function setVarsDelegatecall(address _contract, uint _num) public payable {
         // A's storage is set, B is not modified.
         (bool success, bytes memory data) = _contract.delegatecall(
             abi.encodeWithSignature("setVars(uint256)", _num)
@@ -28,6 +25,16 @@ contract A {
 
         require( success && data.length == 0);
     }
-}
 
-/* A使用委托调用，改变的是自己的状态，B不受影响*/
+    function setVarsCall(address _contract, uint _num) public payable {
+        //B's storage is set, A is not modified.
+        (bool success, bytes memory data) = _contract.call(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+
+        require( success && data.length == 0);
+    }
+}
+/* A使用委托调用，改变的是自己的状态，B不受影响
+    使用普通调用，改变了B的状态，自身不受影响
+*/
